1556a7a337f6f8cc780d879dc73143791006602f
Jason Cox <jasongenecox@gmail.com>
2018-03-29 23:15:22 -0500

    Update and rename villa_krr/villa_kb/interface.cpp to villa_krr/villa_kb/src/interface.cpp

--- src/interface.cpp
+++ src/interface.cpp
@@ -10,19 +10,13 @@ using namespace std;
 
 
 kb_interface::kb_interface(std::string addr, int port, std::string usr, std::string password, std::string db_name) {
-
-
+	
 		try {
-
-				SessionSettings from_options(addr, port, usr, password, db_name);
-
-				cout << "Creating session from_options: " << endl;
-				sess = new Session(from_options);
-				cout <<"Session accepted, creating schema." <<endl;
-				db = new Schema(*sess, db_name);
-
-			
-
+			SessionSettings from_options(addr, port, usr, password, db_name);
+			cout << "Creating session from_options: " << endl;
+			sess = new Session(from_options);
+			cout <<"Session accepted, creating schema." <<endl;
+			db = new Schema(*sess, db_name);
 		}
 		catch (const mysqlx::Error &err) {
 		  cout <<"ERROR: " <<err <<endl;
@@ -48,7 +42,6 @@ kb_interface::~kb_interface() {
 }
 
 
-
 void kb_interface::add_object(int id, std::string attribute_name, int attribute_value_object_id, std::string attribute_value_string, float attribute_value_float, bool attribute_value_bool) {
 
 		/*Note: This method acts also as an 'add object relation' method.
@@ -59,11 +52,7 @@ void kb_interface::add_object(int id, std::string attribute_name, int attribute_
 		 * Also, it is implicit that attribute_name is one of {'concept', 'is_a', 'is_on', 'is_in'.}
 		 * Don't try to use something else. */	
 
-			
-
 		// First, the actual object ID must be added to the 'objects' table if it is a completely new object.
-
-
 		Table objects = db->getTable("objects");
 		TableInsert inserter = objects.insert();
 		RowResult result = objects.select().execute();
@@ -77,29 +66,18 @@ void kb_interface::add_object(int id, std::string attribute_name, int attribute_
 		if(!exists) {
 			inserter.values(id);
 			inserter.execute();
-		
-		
 		}
-
-
-
-
-
 
 
 		Table object_attributes = db->getTable("object_attributes");
 		
-		// Then, attributes must be added to object_attributes table.
-		
-		
-		
+		// Then, attributes must be added to object_attributes table.		
 		if(attribute_name == "concept") {
-
 			inserter = object_attributes.insert("object_id", "attribute_name", "attribute_value_string");
 			inserter.values(id, attribute_name, attribute_value_string);
 		}
-		else {
-		
+	
+		else {	
 			inserter = object_attributes.insert("object_id", "attribute_name", "attribute_value_object_id");
 			inserter.values(id, attribute_name, attribute_value_object_id);
 
@@ -127,16 +105,11 @@ void kb_interface::update_object(int id, std::string attribute_name, int attribu
 	// Note: This function works fine, but if you have null values in the database, calling those will get rid of them and replace them with default values
 	// e.g. 0.0 for float or 0 for bool. This will need to be fixed in the future.
 
-
 }	
 
 
-
-
-
 void kb_interface::delete_object(int id) {
-
-
+	
 		// First, removing all references to the object
 		Table table = db->getTable("object_attributes");
 		TableRemove remover = table.remove();
@@ -144,13 +117,9 @@ void kb_interface::delete_object(int id) {
 		remover.where("attribute_value_object_id=\"" + to_string(id) + "\"");
 		remover.execute();
 
-
-
 		// Next, Removing entry from the original objects table	
 		remover.where("object_id=\"" + to_string(id) + "\"");
 		remover.execute();
-
-
 
 		// Finally, removing entry from object_attributes table
 		table = db->getTable("objects");
