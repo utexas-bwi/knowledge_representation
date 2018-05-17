5fc9af77f0342cf8c79d86bc4e54235913024db2
Nick Walker <nick@nickwalker.us>
2018-05-17 10:30:58 -0500

    Add more comprehensive planner test initialization
    Rename plan executor file
    
    Co-authored-by: Yuqian Jiang <jiangyuqian@utexas.edu>

--- src/initialize_planner_test_configuration.cpp
+++ src/initialize_planner_test_configuration.cpp
@@ -8,52 +8,70 @@ using ::std::endl;
 using namespace std;
 
 
-int main(int argc, const char *argv[]) {
+int main(int argc, char *argv[]) {
+    ros::init(argc, argv, "test_planner");
     knowledge_rep::MemoryConduit mc;
     knowledge_rep::LongTermMemoryConduit ltmc("127.0.0.1", 33060, "root", "", "villa_krr");
 
-    // The following chain of operations creates an object with id 900 that is a coke and exists in living room 100.
-    // Then it deletes it (if you uncomment that line).
-    // To view the results, make sure you are connected to your server and try:
-    //
-    // 		using villa_krr;
-    // 		select * from objects;
-    // 		select * from object_attributes;
-    //
-    // if you try this before and after you will see that the object and its relations have indeed been added.
 
     /*
-     *
-     * object(1).
-graspable(1).
-object(2).
-surface(2).
-is_on(1, 2, 0).
-navigable(2).
-person(3).
-object(4).
-navigable(4).
-is_near(3, 4).
+    object(2). %concept corridor
+    concept(2, corridor).
+    object(3). %corridor
+    is_a(3, 2).
+    navigable(3).
+    object(4). %concept kitchen
+    concept(4, kitchen).
+    object(5). %kitchen
+    is_a(5, 4).
+    object(6). %concept surface
+    concept(6, surface).
+    object(7). %concept table
+    concept(7, table).
+    is_a(7, 6).
+    object(8). %table
+    is_a(8, 7).
+    navigable(8).
+    is_in(8, 5).
+    object(9). %concept coke
+    concept(9, coke).
 
-hand_empty(0).
-at(2, 0).
+    at(3).
 
-     */
+    %added by state machine
+    object(10). %coke
+    is_a(10, 9).
+    can_be_in_concept_id(9, 4).*/
+
     ltmc.delete_all_objects();
     assert(ltmc.get_all_objects().size() == 1);
-    int coke = ltmc.add_object();
-    ltmc.add_object_attribute(coke, "graspable", true);
+    int corridor_concept = ltmc.add_object();
+    ltmc.add_object_attribute(corridor_concept, "concept", "corridor");
+    int corridor = ltmc.add_object();
+    ltmc.add_object_attribute(corridor, "is_a", corridor_concept);
+    ltmc.add_object_attribute(corridor, "navigable", true);
+    int kitchen_concept = ltmc.add_object();
+    ltmc.add_object_attribute(kitchen_concept, "concept", "kitchen");
+    int kitchen = ltmc.add_object();
+    ltmc.add_object_attribute(kitchen, "is_a", kitchen_concept);
+    int surface_concept = ltmc.add_object();
+    ltmc.add_object_attribute(surface_concept, "concept", "surface");
+    int table_concept = ltmc.add_object();
+    ltmc.add_object_attribute(table_concept, "concept", "table");
+    ltmc.add_object_attribute(table_concept, "is_a", surface_concept);
     int table = ltmc.add_object();
-    ltmc.add_object_attribute(table, "surface", true);
+    ltmc.add_object_attribute(table, "is_a", table_concept);
     ltmc.add_object_attribute(table, "navigable", true);
-    ltmc.add_object_attribute(coke, "is_on", table);
-    int person = ltmc.add_object();
-    ltmc.add_object_attribute(person, "person", true);
-    int bed = ltmc.add_object();
-    ltmc.add_object_attribute(bed, "navigable", true);
-    ltmc.add_object_attribute(person, "is_near", bed);
-    //bool success = mc.encode_internal_state();
-    //assert(success);
+    ltmc.add_object_attribute(table, "is_in", kitchen);
+    int coke_concept = ltmc.add_object();
+    ltmc.add_object_attribute(coke_concept, "concept", "coke");
+
+    ltmc.add_object_attribute(corridor, "at", true);
+
+    int coke = ltmc.add_object();
+    ltmc.add_object_attribute(coke, "is_a", coke_concept);
+    ltmc.add_object_attribute(coke_concept, "can_be_in_concept_id", kitchen_concept);
+    
     cout << "Done!" << endl;
 } 
 
